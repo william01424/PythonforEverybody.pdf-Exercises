57771f269983df0483636995f17343039f8b2e80
Will Templeton <75176144+william01424@users.noreply.github.com>
2020-12-15 21:09:16 +0000

    Exercise 3

---
+++ pythonlearn_ex3.py
@@ -0,0 +1,13 @@
+x = int(input("Enter your hours worked:\n"))
+y = int(input("Enter your hourly rate:\n"))
+
+if x > 40:
+    high_rate_hours = float(x - 40)
+    bonus_rate_pay = float(high_rate_hours * y * 1.5)
+    print("Your total pay (including bonus rate over 40 hours) is: £", "{:.2f}".format(((40 * y) + bonus_rate_pay)))
+else:
+    x <= 40
+    base_rate_pay = float(x * y)
+    print("Your total pay earned is: £", "{:.2f}".format(base_rate_pay))
+
+
