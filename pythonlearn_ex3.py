4ed1965dc052b7a6b990fe4383d5d3f0995d0635
Will Templeton <75176144+william01424@users.noreply.github.com>
2020-12-17 19:43:32 +0000

    try / except error message added

--- pythonlearn_ex3.py
+++ pythonlearn_ex3.py
@@ -11,3 +11,20 @@ else:
     print("Your total pay earned is: £", "{:.2f}".format(base_rate_pay))
 
 
+# Adding in error message using try / except
+
+try:
+    x = float(input("Enter your hours worked:\n"))
+    y = float(input("Enter your hourly rate:\n"))
+except ValueError:
+    print("Error, please enter a numeric input")
+    quit()
+
+    if x > 40:
+        high_rate_hours = float(x - 40)
+        bonus_rate_pay = float(high_rate_hours * y * 1.5)
+        print("Your total pay (including bonus rate over 40 hours) is: £", "{:.2f}".format(((40 * y) + bonus_rate_pay)))
+    else:
+        x <= 40
+        base_rate_pay = float(x * y)
+        print("Your total pay earned is: £", "{:.2f}".format(base_rate_pay))
