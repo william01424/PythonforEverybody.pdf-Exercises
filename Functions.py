61de12271727112a8ac57ceae3db07f9a8ad3a7a
Will Templeton <75176144+william01424@users.noreply.github.com>
2021-01-16 22:12:41 +0000

    Functions Completed

--- Functions.py
+++ Functions.py
@@ -60,7 +60,64 @@ def repeat_song():
     print_song()
 repeat_song()
 
-def repeat_major():
-    repeat_song()
-    repeat_song()
-repeat_major()
+def print_twice(bruce):
+    print(bruce)
+    print(bruce)
+
+print_twice('bruce')
+print_twice('spam')
+print_twice('17274')
+
+
+print_twice('spam '*4)          # Here - the code is going to print spam\n spam\n, multiplying each line by 4
+
+print_twice(math.cos(math.pi))
+
+# An example here of how a variable can be used as an argument:
+
+William = 'William is a seriously cool dude.'
+print_twice(William)
+
+# Fruitful and Void Functions
+
+# When calling a fruitful function - you will almost always want to do something with it
+
+radians = 0.7
+
+x = math.cos(radians)
+golden = (math.sqrt(5) + 1)/2
+
+# If you call a function in a script and do not store - the result disappears!?!?! MADNESS
+
+math.sqrt(3) # - result is computed but not stored, therefore not useful
+
+# Void functions may display something on the screen or have another effect, but they don't return a value
+# Try assigning the result to a variable and see what you get
+
+result = print_twice('Bing')
+
+print(result)
+
+# This brings us on to how to return a result from a function - using the return statement
+
+def addtwonumbers(a, b):
+    added = a+b
+    return added
+
+x = addtwonumbers(5,10)
+print(x)
+
+# Steps of this simple program to add two numbers:
+# Computes the sum of the 2 values given
+# Placed this value in the local variable which is called added
+# Then return statement is used to send the computed value back to the calling code as the function result
+# This was assigned to variable x then printed
+
+def fred():
+    print("Zap")
+def jane():
+    print("ABC")
+
+jane()
+fred()
+jane()
